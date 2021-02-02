cf6f59576d70b7950d7cf51225df3a5b3264fd45
KokiHirokawa <kitakitsune1110@gmail.com>
2021-02-03 00:16:53 +0900

    42_硬貨の問題

---
+++ book/42_coin_problem.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+int main() {
+    const vector<int> v = {1, 5, 10, 50, 100, 500};
+
+    vector<int> c(6);
+    rep2(i, 6) cin >> c[i];
+
+    int a;
+    cin >> a;
+
+    int res = 0;
+    for (int i = 5; i >= 0; i--) {
+        int count = min(a / v[i], c[i]);
+        a -= v[i] * count;
+        res += count;
+    }
+
+    printf("%d\n", res);
+    return 0;
+}
