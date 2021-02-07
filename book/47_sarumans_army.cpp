b37d7d5fcd76120036a542379423494e25118ffa
KokiHirokawa <kitakitsune1110@gmail.com>
2021-02-07 11:41:02 +0900

    47_sarumans_army

---
+++ book/47_sarumans_army.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+int main() {
+    int n, r;
+    cin >> n >> r;
+
+    vector<int> x(n);
+    rep2(i, n) cin >> x[i];
+
+    sort(x.begin(), x.end());
+
+    int l = 0, res = 0;
+    while (l < n) {
+        int s = x[l++];
+        while (l < n && x[l] <= s + r) l++;
+
+        int p = x[l - 1];
+        while (l < n && x[l] <= p + r) l++;
+
+        res++;
+    }
+
+    printf("%d\n", res);
+    return 0;
+}
