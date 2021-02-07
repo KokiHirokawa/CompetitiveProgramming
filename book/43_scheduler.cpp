a93eb02bef2d0b8df6bba09dc838254a21897771
KokiHirokawa <kitakitsune1110@gmail.com>
2021-02-07 11:01:23 +0900

    43_scheduler

---
+++ book/43_scheduler.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> s(n), t(n);
+    rep2(i, n) cin >> s[i];
+    rep2(i, n) cin >> t[i];
+
+    pair<int, int> p[n];
+    rep2(i, n) {
+        p[i].first = t[i];
+        p[i].second = s[i];
+    }
+    sort(p, p + n);
+
+    int res = 0, time = 0;
+    rep2(i, n) {
+        if (time < p[i].second) {
+            time = p[i].first;
+            res++;
+        }
+    }
+    printf("%d\n", res);
+    return 0;
+}
