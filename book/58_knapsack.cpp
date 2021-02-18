da84452f3dc46e0d4daa9828ed84ff506eec7769
KokiHirokawa <kitakitsune1110@gmail.com>
2021-02-19 00:06:48 +0900

    58_knapsack

---
+++ book/58_knapsack.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> w(n), v(n);
+    rep2(i, n) cin >> w[i] >> v[i];
+
+    int limit;
+    cin >> limit;
+
+    vector<vector<int>> dp(n + 1, vector<int>(limit + 1, 0));
+
+    rep2(i, n) erep2(j, limit) {
+        for (int k = 0; k * w[i] <= j; k++) {
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - k * w[i]] + k * v[i]);
+        }
+    }
+
+    printf("%d\n", dp[n][limit]);
+    return 0;
+}
