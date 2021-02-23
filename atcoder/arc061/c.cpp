7d72bee77a07dd1cc58a514d10e1fd24bfe0b07b
KokiHirokawa <kitakitsune1110@gmail.com>
2021-02-23 11:03:58 +0900

    arc061 c

---
+++ atcoder/arc061/c.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+string s;
+
+ll dfs(int idx, int prev, ll sum) {
+    string source = s.substr(prev, idx - prev + 1);
+    if (idx == s.length() - 1) {
+        return sum + stoll(source);
+    } else {
+        return dfs(idx + 1, prev, sum) + dfs(idx + 1, idx + 1, sum + stoll(source));
+    }
+}
+
+int main() {
+    cin >> s;
+    printf("%lld\n", dfs(0, 0, 0));
+    return 0;
+}
