75340c8f44b429b719656e760f7c505fcb4d5e95
KokiHirokawa <kitakitsune1110@gmail.com>
2021-03-04 00:21:19 +0900

    111_prime_number

---
+++ book/111_prime_number.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+int main() {
+    ll n;
+    cin >> n;
+
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    ll res = 0;
+
+    rep3(i, 2, n + 1) {
+        if (isPrime[i]) res++;
+        else continue;
+
+        ll j = i * 2;
+        while (j < n + 1) {
+            isPrime[j] = false;
+            j += i;
+        }
+    }
+
+    printf("%lld\n", res);
+    return 0;
+}
