48dc9cd2e3ce4a67860d768f5b72588496fd05ad
KokiHirokawa <kitakitsune1110@gmail.com>
2021-03-07 16:20:50 +0900

    75 fence repair

---
+++ book/75_fence_repair.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+int main() {
+    ll n;
+    cin >> n;
+
+    priority_queue<
+        ll,
+        vector<ll>,
+        greater<ll>
+    > que;
+
+    rep2(i, n) {
+        ll temp;
+        cin >> temp;
+        que.push(temp);
+    }
+
+    ll cost = 0;
+    while (que.size() > 1) {
+        ll a = que.top();
+        que.pop();
+
+        ll b = que.top();
+        que.pop();
+
+        ll sum = a + b;
+        cost += sum;
+
+        que.push(sum);
+    }
+
+    print(cost);
+    return 0;
+}
