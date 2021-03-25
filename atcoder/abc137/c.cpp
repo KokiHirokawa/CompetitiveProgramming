ec765c2d36eb0c3de78cc5bbe4ff24ff99235cb7
KokiHirokawa <kitakitsune1110@gmail.com>
2021-03-25 09:36:29 +0900

    abc137 c

---
+++ atcoder/abc137/c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<string, ll> m;
+
+    rep2(i, n) {
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        m[s]++;
+    }
+
+    ll res = 0;
+    for (auto itr = m.begin(); itr != m.end(); ++itr) {
+        ll second = itr->second;
+        res += second * (second - 1) / 2;
+    }
+    printf("%lld\n", res);
+    return 0;
+}
