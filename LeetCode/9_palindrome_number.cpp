3d14b45ce845386c5cb83366affb8b40dfc52cf6
KokiHirokawa <kitakitsune1110@gmail.com>
2022-01-23 23:46:52 +0900

    LeetCode 9

---
+++ LeetCode/9_palindrome_number.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define erep(n) for (ll i = 0; i <= n; ++i)
+#define erep2(i, n) for (ll i = 0; i <= n; ++i)
+#define erep3(i, a, b) for (ll i = a; i <= b; ++i)
+#define erep4(i, a, b, c) for (ll i = a; i <= b; i += c)
+#define rrep(i, a, b) for (ll i = a; i > b; --i)
+#define rrep2(i, a, b, c) for (ll i = a; i > b; i -= c)
+#define errep(i, a, b) for (ll i = a; i >= b; --i)
+#define errep2(i, a, b, c) for (ll i = a; i >= b; i -= c)
+#define print(a) cout << a << endl;
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define cin(x) cin >> x;
+const ll MOD = 1000000007;
+const double PI = 3.14159265358979323846264338327950;
+inline int newline() { putchar('\n'); return 0; }
+
+class Solution {
+    public:
+        bool isPalindrome(int x) {
+            string s = to_string(x);
+            auto size = s.size();
+            rep((s.size() + 1) / 2) {
+                if (s[i] != s[size - 1 - i]) return false;
+            }
+            return true;
+        }
+};
+
+int main() {
+    Solution s;
+    cout << s.isPalindrome(121) << endl;
+    cout << s.isPalindrome(-121) << endl;
+    cout << s.isPalindrome(10) << endl;
+
+    return 0;
+}
